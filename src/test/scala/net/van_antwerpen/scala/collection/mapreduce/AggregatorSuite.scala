45eb3cdb50c1518e67fd8fee96c2fe95b08ce45c
Hendrik van Antwerpen <hendrik@van-antwerpen.net>
2012-11-28 23:01:11 +0100

    Added license info.

--- src/test/scala/net/van_antwerpen/scala/collection/mapreduce/AggregatorSuite.scala
+++ src/test/scala/net/van_antwerpen/scala/collection/mapreduce/AggregatorSuite.scala
@@ -1,9 +1,21 @@
+/* Copyright 2012 Hendrik van Antwerpen
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.van_antwerpen.scala.collection.mapreduce
 
-import org.scalatest.FunSuite
-
 import scala.collection.immutable._
-
+import org.scalatest.FunSuite
 import Aggregator._
 
 class AggregatorSuite extends FunSuite {
